1f913fcbfc89635dafff352b341b1d185d2a2499
Mukesh <msahu201989@gmail.com>
2022-12-10 21:22:40 +0530

    Rabbit MQ

---
+++  vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "subnets" {}
+variable "name" {}
+variable "instance_type" {}
+#variable "private_zone_id" {}
+#variable "BASTION_NODE" {}
+#variable "vpc_id" {}
+#variable "vpc_cidr" {}
---
+++ data.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "centos-8-ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+#data "aws_secretsmanager_secret" "roboshop" {
+#  name = "roboshop/all-secrets"
+#}
+#
+#data "aws_secretsmanager_secret_version" "roboshop" {
+#  secret_id = data.aws_secretsmanager_secret.roboshop.id
+#}
+#
