2d5935d74a79404d7f248e15cdb3e80fd59e0e47
Mukesh <msahu201989@gmail.com>
2022-12-15 19:11:07 +0530

    listners

---  vars.tf
+++  vars.tf
@@ -2,7 +2,7 @@ variable "env" {}
 variable "subnets" {}
 variable "name" {}
 variable "instance_type" {}
-#variable "private_zone_id" {}
+variable "private_zone_id" {}
 #variable "BASTION_NODE" {}
 #variable "vpc_id" {}
 #variable "vpc_cidr" {}
