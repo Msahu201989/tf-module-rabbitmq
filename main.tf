802239b9ae4b32c58ebe79b287e33f336e1fbab5
Mukesh <msahu201989@gmail.com>
2022-12-15 19:41:10 +0530

    listners

--- main.tf
+++ main.tf
@@ -2,7 +2,24 @@ resource "aws_instance" "rabbitmq" {
   ami                    = data.aws_ami.centos-8-ami.image_id
   instance_type          = var.instance_type
   subnet_id              = var.subnets[0]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "${var.env}-${var.name}-rabbitmq"
+  }
+}
+
+resource "null_resource" "ansible-apply" {
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_instance.rabbitmq.private_ip
+      user     = local.SSH_USER
+      password = local.SSH_PASS
+    }
+
+    inline = [
+      "sudo yum install python39-devel -y",
+      "sudo pip3.9 install ansible botocore boto3",
+      "ansible-pull -i localhost, -U https://github.com/raghudevopsb67/roboshop-ansible roboshop.yml -e ROLE_NAME=rabbitmq -e ENV=${var.env}"
+    ]
   }
 }
